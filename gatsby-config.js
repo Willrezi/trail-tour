2b937c8ad0738458c50ea501b447147983df2f88
Wilfried LLOZA <wilfried.lloza@gmail.com>
2020-04-29 16:52:19 +0200

    add gatsby plugin sass & google fonts & react-icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,34 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Trail Tour",
+    author: "Wilfried Lloza",
+    description: "Des sentiers à découvrir",
+  },
+
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          {
+            family: "Oswald",
+            variants: ["400", "700"],
+            // subsets: [`latin`],
+          },
+          {
+            family: "Open Sans",
+            variants: ["400", "700"],
+          },
+          {
+            family: "Catamaran",
+            variants: ["300", "400", "700"],
+          },
+        ],
+      },
+    },
+  ],
 }
