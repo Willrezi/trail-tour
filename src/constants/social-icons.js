c71e813c5b71b30d79aeeacfccbe67426046ea60
Wilfried LLOZA <wilfried.lloza@gmail.com>
2020-05-04 18:04:52 +0200

    create constants file

---
+++ src/constants/social-icons.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { FaFacebook, FaTwitterSquare, FaInstagram } from "react-icons/fa"
+
+const SocialIcons = [
+  {
+    icon: <FaFacebook />,
+    url: "https://facebook.com",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https://twitter.com",
+  },
+  {
+    icon: <FaInstagram />,
+    url: "https://www.instagram.com",
+  },
+]
+export default SocialIcons
