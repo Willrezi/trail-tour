2b9c05b63348fca479dbf29496de5818d3fde025
Wilfried LLOZA <wilfried.lloza@gmail.com>
2020-05-14 15:25:17 +0200

    add offer component

---
+++ src/components/offer.js
@@ -0,0 +1,23 @@
+import React from "react"
+import OfferIcons from "../constants/offer-icons"
+
+const Offer = () => {
+  return (
+    <section className="offer-section">
+      <h3 className="section-title">Notre offre</h3>
+      <div className="offer-block">
+        {OfferIcons.map(item => {
+          return (
+            <div className="offer-detail">
+              <span>{item.icon}</span>
+              <h4>{item.title}</h4>
+              <p>{item.text}</p>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Offer
---
+++ src/constants/offer-icons.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { FaHiking, FaRunning, FaBiking } from "react-icons/fa"
+
+const OfferIcons = [
+  {
+    icon: <FaHiking />,
+    title: "Randonnée",
+    text:
+      "Le Haut-Doubs, en plein cœur des Montagnes du Jura et aux portes de la Suisse, est le point de départ idéal pour des balades aux vues à couper le souffle et le terrain de jeu rêvé pour les amateurs de grande randonnée.",
+  },
+  {
+    icon: <FaRunning />,
+    title: "Trail Running",
+    text:
+      "En plein cœur des Montagnes du Jura, le Pays du Haut-Doubs est un terrain de jeu idéal pour le Trail Running, cette discipline qui allie effort, dépassement de soi et contemplation. Si vous aimez découvrir les paysages à petites ou grandes foulées, vous ne serez pas déçus. Choisi en fonction de vos attentes et vos envies, chaque circuit vous offrira son lot de sensations.",
+  },
+  {
+    icon: <FaBiking />,
+    title: "VTT",
+    text:
+      "Partez à la découverte de la faune : chamois, chevreuils, lièvres ... et de la flore : gentiane jaune ou bleue, lys martagon ... de nos Montagnes du Jura en VTT !",
+  },
+]
+
+export default OfferIcons
