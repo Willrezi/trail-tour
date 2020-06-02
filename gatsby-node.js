cb1edab68596e076e6d6bd732283824d49ec079f
Wilfried LLOZA <wilfried.lloza@gmail.com>
2020-06-02 16:01:40 +0200

    create node file

---
+++ gatsby-node.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/posts.js")
+  const res = await graphql(`
+    query {
+      allContentfulToursData {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allContentfulToursData.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+}
